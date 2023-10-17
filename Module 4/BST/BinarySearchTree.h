88e5aa7d45dad3270e7737ee7112887baa71077c
Prof Vickers <dennis.vickers@outlook.com>
2023-10-17 16:49:13 -0700

    Initial BST

---
+++ Module 4/BST/BinarySearchTree.h
@@ -0,0 +1,27 @@
+#ifndef BST_BINARYSEARCHTREE_H
+#define BST_BINARYSEARCHTREE_H
+
+struct Node {
+   int data;
+   Node *left;
+   Node *right;
+   Node(int val);
+};
+
+class BinarySearchTree {
+public:
+   BinarySearchTree();
+   ~BinarySearchTree();
+   void insert(int val);
+   bool search(int val);
+   void remove(int val);
+   void showInOrder();
+private:
+   Node *root;
+   void removeHelper(Node *&root, int val);
+   Node *findMin(Node *root);
+   void showInOrderHelper(Node *root);
+   void deleteTree(Node *root);
+};
+
+#endif //BST_BINARYSEARCHTREE_H
