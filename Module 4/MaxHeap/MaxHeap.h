e54166fc12b45f66d658b4f6ffd7fe145327c8ff
Prof Vickers <dennis.vickers@outlook.com>
2023-10-27 08:23:24 -0700

    Update

--- Module 4/MaxHeap/MaxHeap.h
+++ Module 4/MaxHeap/MaxHeap.h
@@ -1,12 +1,16 @@
 #ifndef MAXHEAP_MAXHEAP_H
 #define MAXHEAP_MAXHEAP_H
 
-#include "MaxHeap.h"
 #include <vector>
 #include <stdexcept>
 using namespace std;
 
 class MaxHeap {
+public:
+   void insert(int value);    // Add an element on the heap
+   int deleteMax();           // Remove and return the maximum element
+   int peekMax() const;       // Return the maximum element without removing it
+
 private:
    vector<int> heap;
 
@@ -16,10 +20,6 @@ private:
    int rightChild(int i) { return 2 * i + 2; };  // Get right child index
    void moveUp(int i);                           // Percolate an index up the heap
    void moveDown(int i);                         // Move an index down the heap
-public:
-   void insert(int value);    // Add an element on the heap
-   int deleteMax();           // Remove and return the maximum element
-   int peekMax() const;       // Return the maximum element without removing it
 };
 
 #endif //MAXHEAP_MAXHEAP_H
